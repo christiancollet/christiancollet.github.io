2b3e71a9db8c57a3f28a613f4593759d78a31afb
christiancollet <30608433+christiancollet@users.noreply.github.com>
2025-10-11 12:04:39 +0000

    Deploying to gh-pages from @ christiancollet/christiancollet.github.io@a4a1c3501ac3c62ce318d5a5fc191684bb57d530 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "christiancollet/collet.github.io",
+    "data-repo": "christiancollet/christiancollet.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
